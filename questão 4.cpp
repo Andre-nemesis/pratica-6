4c270f7a3e30bd392dec636ba00ec0b293a73187
Andre-nemesis <105876843+Andre-nemesis@users.noreply.github.com>
2023-02-02 14:19:25 -0300

    Update questão 4.cpp

--- questão 4.cpp
+++ questão 4.cpp
@@ -2,31 +2,25 @@
 #include <stdlib.h>
 #define max 500
 
-void invarray (char ary[],int tam) {
-	int i, metade = tam/2;
-	char aux;
-	
-	for(i = 0;i < metade;i++) {
-		aux = ary[i];
-		ary[i] = ary[tam - i - 1];
-		ary[tam - i - 1] = aux;
-	}
-}
-
 int main () {
-	char frase[max];
+	char frase[max],aux;
 	int tamfra,c;
 	
 	printf("Digite uma palavra ou uma frase: ");
 	gets(frase);
 	
-	for(c = 0;frase[c] != '\0';c++) {
+	for(c = 0;c < max;c++) {
 		tamfra = c - 1;
+		if(frase[c] == '\0') break;
 	}
 	
-	invarray(frase,tamfra);
-	
 	printf("Frase ao contrario:\n");
+	for(c = 0; c <= tamfra/2;c++) {
+		aux = frase[c];
+		frase[c] = frase[tamfra - c];
+		frase[tamfra - c] = aux;
+	}
+	
 	puts(frase);
 	
 	return 0;
